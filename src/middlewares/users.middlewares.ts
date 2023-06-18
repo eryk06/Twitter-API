f0d8d90f251bed76755f2fde8660db02c100d1d8
Erykdev06 <lamnguyen062003@gmail.com>
2023-06-18 23:33:46 +0700

    add file

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
+import { checkSchema } from 'express-validator'
+import { validate } from '~/utils/validation'
 
 export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body
@@ -7,3 +9,73 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction) 
   }
   next()
 }
+
+export const registerValidator = validate(
+  checkSchema({
+    name: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: { min: 1, max: 100 }
+      },
+      trim: true
+    },
+    email: {
+      notEmpty: true,
+      isEmail: true,
+      trim: true
+    },
+    password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: { min: 6, max: 50 }
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        }
+      },
+      trim: true,
+      errorMessage: 'Password must be at least 6 characters'
+    },
+    confirm_password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: { min: 6, max: 50 }
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        }
+      },
+      trim: true,
+      errorMessage: 'Password must be at least 6 characters',
+      custom: {
+        options: (value, { req }) => {
+          if (value !== req.body.password) {
+            throw new Error('Passwords do not match')
+          }
+          return true
+        }
+      }
+    },
+    date_of_birth: {
+      isISO8601: {
+        options: {
+          strict: true,
+          strictSeparator: true
+        }
+      }
+    }
+  })
+)
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express'
 import { loginController, registerController } from '~/controllers/users.controllers'
-import { loginValidator } from '~/middlewares/users.middlewares'
+import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
 
 const usersRouter = Router()
 
 usersRouter.post('/login', loginValidator, loginController)
-usersRouter.post('/register', registerController)
+
+/**
+ * TODO: Description. Register a new user
+ * Path: /register
+ * Method: POST
+ * Body: { name: string, email: string, password: string, confirm_password: string, date_of_birth: ISO8601 }
+ */
+
+usersRouter.post('/register', registerValidator, registerController)
 
 export default usersRouter
